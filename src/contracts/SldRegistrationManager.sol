e4fc860c32c74dca69220dfc09c81984dae266fe
Sam Ward <sam@piranha.me.uk>
2022-09-14 17:28:41 +0100

    added interface and contract for SldRegistrationManager

--- src/contracts/SldRegistrationManager.sol
+++ src/contracts/SldRegistrationManager.sol
@@ -1,6 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-contract SldRegistrationManager {
-    constructor() {}
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SldRegistrationManager is Ownable {
+    constructor() {
+
+
+
+    }
+
+        function purchaseMultipleSld(
+        string[] calldata _label,
+        bytes32[] calldata _secret,
+        uint256[] calldata _registrationLength,
+        bytes32[] calldata _parentNamehash,
+        address[] calldata _recipient
+    ) public payable { }
 }
---
+++ src/interfaces/ISldRegistrationManager.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+interface ISldRegistrationManager {
+
+        function purchaseMultipleSld(
+        string[] calldata _label,
+        bytes32[] calldata _secret,
+        uint256[] calldata _registrationLength,
+        bytes32[] calldata _parentNamehash,
+        address[] calldata _recipient
+    ) external payable;
+
+        function purchaseSingleDomain(
+        string calldata _label,
+        bytes32 _secret,
+        uint256 _registrationLength,
+        bytes32 _parentNamehash,
+        bytes32[] calldata _proofs,
+        address _recipient
+    ) external payable;
+}
