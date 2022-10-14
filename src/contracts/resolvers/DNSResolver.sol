5d0e203284d7d0c9a176dd26cb9f791bafad0dcb
Sam Ward <sam@piranha.me.uk>
2022-10-14 14:25:51 +0100

    added DNSResolver code and tests

--- src/contracts/resolvers/DNSResolver.sol
+++ src/contracts/resolvers/DNSResolver.sol
@@ -5,42 +5,178 @@ import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "interfaces/resolvers/IDNSZoneResolver.sol";
 import "interfaces/resolvers/IDNSRecordResolver.sol";
 import "contracts/resolvers/BaseResolver.sol";
+import "utils/BytesUtils.sol";
+import "utils/RRUtils.sol";
 
 abstract contract DNSResolver is IDNSRecordResolver, IDNSZoneResolver, BaseResolver {
+    using RRUtils for *;
+    using BytesUtils for bytes;
+
     // Zone hashes for the domains.
     // A zone hash is an EIP-1577 content hash in binary format that should point to a
     // resource containing a single zonefile.
     // node => contenthash
     mapping(uint256 => mapping(bytes32 => bytes)) private versionable_zonehashes;
 
+    // The records themselves.  Stored as binary RRSETs
+    // node => version => name => resource => data
+    mapping(uint256 => mapping(bytes32 => mapping(bytes32 => mapping(uint16 => bytes))))
+        private versionable_records;
+
+    // Count of number of entries for a given name.  Required for DNS resolvers
+    // when resolving wildcards.
+    // node => version => name => number of records
+    mapping(uint256 => mapping(bytes32 => mapping(bytes32 => uint16)))
+        private versionable_nameEntriesCount;
+
+    /**
+     * Set one or more DNS records.  Records are supplied in wire-format.
+     * Records with the same node/name/resource must be supplied one after the
+     * other to ensure the data is updated correctly. For example, if the data
+     * was supplied:
+     *     a.example.com IN A 1.2.3.4
+     *     a.example.com IN A 5.6.7.8
+     *     www.example.com IN CNAME a.example.com.
+     * then this would store the two A records for a.example.com correctly as a
+     * single RRSET, however if the data was supplied:
+     *     a.example.com IN A 1.2.3.4
+     *     www.example.com IN CNAME a.example.com.
+     *     a.example.com IN A 5.6.7.8
+     * then this would store the first A record, the CNAME, then the second A
+     * record which would overwrite the first.
+     *
+     * @param node the namehash of the node for which to set the records
+     * @param data the DNS wire format records to set
+     */
+    function setDNSRecords(bytes32 node, bytes calldata data) external virtual authorised(node) {
+        uint16 resource = 0;
+        uint256 offset = 0;
+        bytes memory name;
+        bytes memory value;
+
+        uint256 version = recordVersions[node];
+        // Iterate over the data to add the resource records
+        for (RRUtils.RRIterator memory iter = data.iterateRRs(0); !iter.done(); iter.next()) {
+            if (resource == 0) {
+                resource = iter.dnstype;
+                name = iter.name();
+
+                value = bytes(iter.rdata());
+            } else {
+                bytes memory newName = iter.name();
+                if (resource != iter.dnstype || !name.equals(newName)) {
+                    setDNSRRSet(
+                        node,
+                        name,
+                        resource,
+                        data,
+                        offset,
+                        iter.offset - offset,
+                        value.length == 0,
+                        version
+                    );
+                    resource = iter.dnstype;
+                    offset = iter.offset;
+                    name = newName;
+
+                    value = bytes(iter.rdata());
+                }
+            }
+        }
+        if (name.length > 0) {
+            setDNSRRSet(
+                node,
+                name,
+                resource,
+                data,
+                offset,
+                data.length - offset,
+                value.length == 0,
+                version
+            );
+        }
+    }
+
+    /**
+     * Obtain a DNS record.
+     * @param node the namehash of the node for which to fetch the record
+     * @param name the keccak-256 hash of the fully-qualified name for which to fetch the record
+     * @param resource the ID of the resource as per https://en.wikipedia.org/wiki/List_of_DNS_record_types
+     * @return the DNS record in wire format if present, otherwise empty
+     */
     function dnsRecord(
         bytes32 node,
         bytes32 name,
         uint16 resource
-    ) external view returns (bytes memory) {
-        require(false, "not implemented");
+    ) public view virtual override returns (bytes memory) {
+        return versionable_records[recordVersions[node]][node][name][resource];
     }
 
     /**
-     * zonehash obtains the hash for the zone.
-     * @param node The ENS node to query.
-     * @return The associated contenthash.
+     * Check if a given node has records.
+     * @param node the namehash of the node for which to check the records
+     * @param name the namehash of the node for which to check the records
      */
-    function zonehash(bytes32 node) public view virtual override returns (bytes memory) {
-        return versionable_zonehashes[recordVersions[node]][node];
+    function hasDNSRecords(bytes32 node, bytes32 name) public view virtual returns (bool) {
+        return (versionable_nameEntriesCount[recordVersions[node]][node][name] != 0);
     }
 
     /**
      * setZonehash sets the hash for the zone.
-     * May only be called by the owner of that node in the ENS registry.
-     * @param node The node to update.
-     * @param newHash The zonehash to set
+     * May only be called by the owner of that node.
+     * @param _node The node to update.
+     * @param _hash The zonehash to set
      */
-    function setZonehash(bytes32 node, bytes calldata newHash) external virtual authorised(node) {
-        uint256 currentRecordVersion = recordVersions[node];
-        bytes memory oldHash = versionable_zonehashes[currentRecordVersion][node];
-        versionable_zonehashes[currentRecordVersion][node] = newHash;
-        emit DNSZonehashChanged(node, oldHash, newHash);
+    function setZonehash(bytes32 _node, bytes calldata _hash) external virtual authorised(_node) {
+        uint256 currentRecordVersion = recordVersions[_node];
+        bytes memory oldhash = versionable_zonehashes[currentRecordVersion][_node];
+        versionable_zonehashes[currentRecordVersion][_node] = _hash;
+        emit DNSZonehashChanged(_node, oldhash, _hash);
+    }
+
+    /**
+     * zonehash obtains the hash for the zone.
+     * @param _node The node to query.
+     * @return The associated contenthash.
+     */
+    function zonehash(bytes32 _node) public view virtual override returns (bytes memory) {
+        return versionable_zonehashes[recordVersions[_node]][_node];
+    }
+
+    function supportsInterface(bytes4 interfaceID) public view virtual override returns (bool) {
+        return
+            interfaceID == type(IDNSRecordResolver).interfaceId ||
+            interfaceID == type(IDNSZoneResolver).interfaceId ||
+            super.supportsInterface(interfaceID);
+    }
+
+    function setDNSRRSet(
+        bytes32 node,
+        bytes memory name,
+        uint16 resource,
+        bytes memory data,
+        uint256 offset,
+        uint256 size,
+        bool deleteRecord,
+        uint256 version
+    ) private {
+        bytes32 nameHash = keccak256(name);
+        bytes memory rrData = data.substring(offset, size);
+        if (deleteRecord) {
+            if (versionable_records[version][node][nameHash][resource].length != 0) {
+                versionable_nameEntriesCount[version][node][nameHash]--;
+            }
+            delete (versionable_records[version][node][nameHash][resource]);
+            emit DNSRecordDeleted(node, name, resource);
+        } else {
+            if (versionable_records[version][node][nameHash][resource].length == 0) {
+                unchecked {
+                    versionable_nameEntriesCount[version][node][nameHash]++;
+                }
+            }
+            versionable_records[version][node][nameHash][resource] = rrData;
+            emit DNSRecordChanged(node, name, resource, rrData);
+        }
     }
 
     function incrementVersion(bytes32 node) public virtual override authorised(node) {
@@ -50,15 +186,10 @@ abstract contract DNSResolver is IDNSRecordResolver, IDNSZoneResolver, BaseResol
 
         bytes memory newHash = zonehash(node);
 
+        //we can only really track the zonehash to output events not the other
+        //dns entries. Just output it if it wasn't blank
         if (keccak256(newHash) != keccak256(oldHash)) {
             emit DNSZonehashChanged(node, oldHash, newHash);
         }
-    }
-
-    function supportsInterface(bytes4 _interfaceId) public view virtual override returns (bool) {
-        return
-            _interfaceId == type(IDNSRecordResolver).interfaceId ||
-            _interfaceId == type(IDNSZoneResolver).interfaceId ||
-            super.supportsInterface(_interfaceId);
     }
 }
--- test/resolvers/TestDNSResolver.t.sol
+++ test/resolvers/TestDNSResolver.t.sol
@@ -6,15 +6,331 @@ import {stdStorage, StdStorage, Test} from "forge-std/Test.sol";
 import "test/mocks/TestResolvers.sol";
 import "test/mocks/MockHandshakeNft.sol";
 
+import "utils/NameEncoder.sol";
+
 contract TestDNSResolver is Test {
+    using NameEncoder for string;
     TestingDNSResolver resolver;
     MockHandshakeNft tld;
     MockHandshakeNft sld;
 
-    function setup() public {
+    function setUp() public {
         tld = new MockHandshakeNft();
         sld = new MockHandshakeNft();
 
         resolver = new TestingDNSResolver(tld, sld);
+    }
+
+    function testSetBasicDNSRecords() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+
+        // a.eth. 3600 IN A 1.2.3.4
+        bytes memory arec = hex"016103657468000001000100000e10000401020304";
+        // b.eth. 3600 IN A 2.3.4.5
+        bytes memory b1rec = hex"016203657468000001000100000e10000402030405";
+        // b.eth. 3600 IN A 3.4.5.6
+        bytes memory b2rec = hex"016203657468000001000100000e10000403040506";
+        // eth. 86400 IN SOA ns1.ethdns.xyz. hostmaster.test.eth. 2018061501 15620 1800 1814400 14400
+        bytes
+            memory soarec = hex"03657468000006000100015180003a036e733106657468646e730378797a000a686f73746d6173746572057465737431036574680078492cbd00003d0400000708001baf8000003840";
+        bytes memory rec = abi.encodePacked(arec, b1rec, b2rec, soarec);
+
+        vm.prank(owner);
+        resolver.setDNSRecords(node, rec);
+
+        string memory aDotEth = "a.eth";
+        string memory bDotEth = "b.eth";
+        string memory ethDot = "eth";
+
+        (bytes memory aName, ) = aDotEth.dnsEncodeName();
+        (bytes memory bName, ) = bDotEth.dnsEncodeName();
+        (bytes memory ethName, ) = ethDot.dnsEncodeName();
+
+        assertEq(
+            resolver.dnsRecord(node, keccak256(aName), 1),
+            hex"016103657468000001000100000e10000401020304"
+        );
+
+        assertEq(
+            resolver.dnsRecord(node, keccak256(bName), 1),
+            hex"016203657468000001000100000e10000402030405016203657468000001000100000e10000403040506"
+        );
+
+        assertEq(
+            resolver.dnsRecord(node, keccak256(ethName), 6),
+            hex"03657468000006000100015180003a036e733106657468646e730378797a000a686f73746d6173746572057465737431036574680078492cbd00003d0400000708001baf8000003840"
+        );
+    }
+
+    function testUpdateDnsRecordsFromOwner() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+
+        bytes memory firstARec = hex"016103657468000001000100000e10000401020306";
+
+        // a.eth. 3600 IN A 1.2.3.4
+        bytes memory arec = hex"016103657468000001000100000e10000401020304";
+
+        // eth. 86400 IN SOA ns1.ethdns.xyz. hostmaster.test.eth. 2018061501 15620 1800 1814400 14400
+        bytes
+            memory soarec = hex"03657468000006000100015180003a036e733106657468646e730378797a000a686f73746d6173746572057465737431036574680078492cbd00003d0400000708001baf8000003840";
+
+        vm.prank(owner);
+        resolver.setDNSRecords(node, abi.encodePacked(firstARec, soarec));
+
+        bytes memory rec = abi.encodePacked(arec, soarec);
+
+        string memory aDotEth = "a.eth";
+        string memory ethDot = "eth";
+
+        (bytes memory aName, ) = aDotEth.dnsEncodeName();
+        (bytes memory ethName, ) = ethDot.dnsEncodeName();
+
+        //original value
+        assertEq(
+            resolver.dnsRecord(node, keccak256(aName), 1),
+            hex"016103657468000001000100000e10000401020306"
+        );
+
+        vm.prank(owner);
+        resolver.setDNSRecords(node, rec);
+
+        assertEq(
+            resolver.dnsRecord(node, keccak256(aName), 1),
+            hex"016103657468000001000100000e10000401020304"
+        );
+
+        assertEq(
+            resolver.dnsRecord(node, keccak256(ethName), 6),
+            hex"03657468000006000100015180003a036e733106657468646e730378797a000a686f73746d6173746572057465737431036574680078492cbd00003d0400000708001baf8000003840"
+        );
+    }
+
+    function testKeepTrackOfEntries() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+
+        string memory cDotEth = "c.eth";
+        string memory dDotEth = "d.eth";
+
+        (bytes memory cName, ) = cDotEth.dnsEncodeName();
+        (bytes memory dName, ) = dDotEth.dnsEncodeName();
+
+        // c.eth. 3600 IN A 1.2.3.4
+        bytes memory crec = hex"016303657468000001000100000e10000401020304";
+
+        vm.startPrank(owner);
+        resolver.setDNSRecords(node, crec);
+
+        assertTrue(resolver.hasDNSRecords(node, keccak256(cName)));
+        assertFalse(resolver.hasDNSRecords(node, keccak256(dName)));
+
+        resolver.setDNSRecords(node, crec);
+
+        //updating makes no difference
+        assertTrue(resolver.hasDNSRecords(node, keccak256(cName)));
+        assertFalse(resolver.hasDNSRecords(node, keccak256(dName)));
+
+        bytes memory crec2 = hex"016303657468000001000100000e100000";
+
+        resolver.setDNSRecords(node, crec2);
+        assertFalse(resolver.hasDNSRecords(node, keccak256(cName)));
+    }
+
+    function testSetSingleRecord() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+
+        string memory eDotEth = "e.eth";
+
+        bytes memory erec = hex"016503657468000001000100000e10000401020304";
+
+        (bytes memory eName, ) = eDotEth.dnsEncodeName();
+
+        vm.startPrank(owner);
+        resolver.setDNSRecords(node, erec);
+
+        assertTrue(resolver.hasDNSRecords(node, keccak256(eName)));
+        assertEq(
+            resolver.dnsRecord(node, keccak256(eName), 1),
+            hex"016503657468000001000100000e10000401020304"
+        );
+    }
+
+    function testSetSingleRecordFromApprovedAddress() public {
+        address owner = address(0x99887766);
+        address approved = address(0x420420);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+
+        string memory eDotEth = "e.eth";
+
+        bytes memory erec = hex"016503657468000001000100000e10000401020304";
+
+        (bytes memory eName, ) = eDotEth.dnsEncodeName();
+
+        vm.prank(owner);
+        sld.setApprovalForAll(approved, true);
+
+        vm.prank(approved);
+        resolver.setDNSRecords(node, erec);
+
+        assertTrue(resolver.hasDNSRecords(node, keccak256(eName)));
+        assertEq(
+            resolver.dnsRecord(node, keccak256(eName), 1),
+            hex"016503657468000001000100000e10000401020304"
+        );
+
+        vm.prank(owner);
+        sld.setApprovalForAll(approved, false);
+
+        vm.expectRevert("not authorised or owner");
+        vm.prank(approved);
+        resolver.setDNSRecords(node, erec);
+    }
+
+    function testSetDnsFromNotApprovedAddress() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+
+        vm.expectRevert("not authorised or owner");
+        resolver.setDNSRecords(node, bytes("irrelevant string"));
+    }
+
+    function testSetDnsZoneHashFromOwner() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+        bytes memory zonehash = hex"04";
+
+        vm.prank(owner);
+        resolver.setZonehash(node, zonehash);
+
+        assertEq(resolver.zonehash(node), zonehash);
+    }
+
+    function testSetDnsZoneHashFromApprovedAddress() public {
+        address owner = address(0x99887766);
+        address approved = address(0x445566);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+        bytes memory zonehash = hex"ab";
+
+        vm.prank(owner);
+        sld.setApprovalForAll(approved, true);
+
+        vm.prank(approved);
+        resolver.setZonehash(node, zonehash);
+
+        assertEq(resolver.zonehash(node), zonehash);
+    }
+
+    function testSetDnsZoneHashFromNotApprovedAddress_fail() public {
+        address owner = address(0x99887766);
+        address notApproved = address(0x445566);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+        bytes memory zonehash = hex"ab";
+
+        vm.expectRevert("not authorised or owner");
+        vm.prank(notApproved);
+        resolver.setZonehash(node, zonehash);
+
+        assertEq(resolver.zonehash(node), bytes(""));
+    }
+
+    function testSetDnsZoneHashAndOverwrite() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+        bytes memory zonehash = hex"04";
+        bytes memory zonehash2 = hex"420690";
+
+        vm.startPrank(owner);
+        resolver.setZonehash(node, zonehash);
+
+        assertEq(resolver.zonehash(node), zonehash);
+
+        resolver.setZonehash(node, zonehash2);
+        assertEq(resolver.zonehash(node), zonehash2);
+    }
+
+    function testReturnsEmptyBytesForNotExistingZonehash() public {
+        assertEq(resolver.zonehash(bytes32(uint256(123456789))), hex"");
+    }
+
+    function testResetDnsOnVersionIncrement() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+
+        string memory eDotEth = "e.eth";
+
+        bytes memory erec = hex"016503657468000001000100000e10000401020304";
+
+        (bytes memory eName, ) = eDotEth.dnsEncodeName();
+
+        vm.startPrank(owner);
+        resolver.setDNSRecords(node, erec);
+
+        assertTrue(resolver.hasDNSRecords(node, keccak256(eName)));
+        assertEq(
+            resolver.dnsRecord(node, keccak256(eName), 1),
+            hex"016503657468000001000100000e10000401020304"
+        );
+
+        assertTrue(resolver.hasDNSRecords(node, keccak256(eName)));
+
+        resolver.incrementVersion(node);
+
+        assertFalse(resolver.hasDNSRecords(node, keccak256(eName)));
+
+        assertEq(resolver.dnsRecord(node, keccak256(eName), 1), hex"");
+    }
+
+    function testResetDnsZoneOnVersionIncrement() public {
+        address owner = address(0x99887766);
+        uint256 id = 696969;
+        sld.mint(owner, id);
+
+        bytes32 node = bytes32(id);
+        bytes memory zonehash = hex"04";
+
+        vm.startPrank(owner);
+        resolver.setZonehash(node, zonehash);
+
+        assertEq(resolver.zonehash(node), zonehash);
+
+        resolver.incrementVersion(node);
+
+        assertEq(resolver.zonehash(node), hex"");
     }
 }
