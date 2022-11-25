3abda052b2a1d9fa007c26f0e08696e5838788b3
Sam <sam@piranha.me.uk>
2022-11-25 12:34:31 +0000

    removed abi and interface resolvers

--- src/contracts/resolvers/DefaultResolver.sol
+++ src/contracts/resolvers/DefaultResolver.sol
@@ -4,12 +4,9 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "contracts/resolvers/BaseResolver.sol";
 import "contracts/HandshakeNft.sol";
-
-import "contracts/resolvers/ABIResolver.sol";
 import "contracts/resolvers/AddressResolver.sol";
 import "contracts/resolvers/ContentHashResolver.sol";
 import "contracts/resolvers/DNSResolver.sol";
-import "contracts/resolvers/InterfaceResolver.sol";
 import "contracts/resolvers/NameResolver.sol";
 import "contracts/resolvers/TextResolver.sol";
 import "interfaces/IResolver.sol";
@@ -17,11 +14,9 @@ import "src/utils/Multicallable.sol";
 
 contract DefaultResolver is
     BaseResolver,
-    ABIResolver,
     AddressResolver,
     ContentHashResolver,
     DNSResolver,
-    InterfaceResolver,
     NameResolver,
     TextResolver,
     Multicallable
@@ -34,11 +29,9 @@ contract DefaultResolver is
         override(
             BaseResolver,
             Multicallable,
-            ABIResolver,
             AddressResolver,
             ContentHashResolver,
             DNSResolver,
-            InterfaceResolver,
             NameResolver,
             TextResolver
         )
--- test/mocks/TestResolvers.sol
+++ test/mocks/TestResolvers.sol
@@ -1,30 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "contracts/resolvers/ABIResolver.sol";
 import "contracts/resolvers/AddressResolver.sol";
 import "contracts/resolvers/ContentHashResolver.sol";
 import "contracts/resolvers/DNSResolver.sol";
-import "contracts/resolvers/InterfaceResolver.sol";
 import "contracts/resolvers/NameResolver.sol";
 import "contracts/resolvers/TextResolver.sol";
 import "src/utils/Multicallable.sol";
 import "contracts/resolvers/BaseResolver.sol";
 import "contracts/HandshakeNft.sol";
 
-contract TestingABIResolver is ABIResolver {
-    constructor(HandshakeNft _tld, HandshakeNft _sld) BaseResolver(_tld, _sld) {}
-}
-
 contract TestingContentHashResolver is ContentHashResolver {
     constructor(HandshakeNft _tld, HandshakeNft _sld) BaseResolver(_tld, _sld) {}
 }
 
 contract TestingDNSResolver is DNSResolver {
-    constructor(HandshakeNft _tld, HandshakeNft _sld) BaseResolver(_tld, _sld) {}
-}
-
-contract TestingInterfaceResolver is InterfaceResolver {
     constructor(HandshakeNft _tld, HandshakeNft _sld) BaseResolver(_tld, _sld) {}
 }
 
