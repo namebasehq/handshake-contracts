35fb5612a2cf55812d196a1dde0989ad38c49e89
Sam <sam@piranha.me.uk>
2023-05-29 21:24:40 +0100

    added create2 deployment contract

---
+++ src/contracts/Factory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+
+// create2 factory
+contract Factory {
+    address public owner;
+    event Deployed(address addr, uint256 salt);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deploy(bytes memory code, uint256 salt) public {
+        require(msg.sender == owner, "Only the contract owner can deploy");
+
+        address addr;
+        assembly {
+            addr := create2(0, add(code, 0x20), mload(code), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+
+        emit Deployed(addr, salt);
+    }
+}
