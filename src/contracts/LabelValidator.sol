06b4c6e6cb153f9379f213a93256d386f10f68dc
Aaron Oxborrow <aaron@oxborrow.com>
2024-05-15 01:54:51 -0700

    Update LabelValidator to allow Punycode (#47)

--- src/contracts/LabelValidator.sol
+++ src/contracts/LabelValidator.sol
@@ -8,9 +8,8 @@ contract LabelValidator is ILabelValidator {
     uint256 public constant MAX_LABEL_LENGTH = 63;
 
     /**
-     * Currently using Handshake/DNS validation rules -- except that we're not allowing punycode or underscores for now.
-     * We have chosen to limit all names to alphanumeric ASCII to start, in order to avoid issues with EPP and existing registrars.
-     * In the future we may enable full Unicode, possibly with on-chain punycoding for backwards compatibility.
+     * Currently using Handshake/DNS validation rules -- except that we're not allowing underscores which are not commonly supported in domains.
+     * We have chosen to limit all names to alphanumeric ASCII, hyphens, and punycode in order to avoid compatibility issues with EPP and existing registrars.
      * https://github.com/handshake-org/hsd/blob/master/lib/covenants/rules.js#L183
      * https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax,_internationalization
      * @param _label The label to validate. Should be normalized to lowercase without spaces, periods, or non-alphanumeric characters.
@@ -24,7 +23,7 @@ contract LabelValidator is ILabelValidator {
         // hyphen not allowed at end or beginning
         if (labelBytes[0] == 0x2D || labelBytes[bytesLength - 1] == 0x2D) return false;
         // double hyphen not allowed at positions 3 & 4, this prevents punycode
-        if (bytesLength > 3 && labelBytes[2] == 0x2D && labelBytes[3] == 0x2D) return false;
+        // if (bytesLength > 3 && labelBytes[2] == 0x2D && labelBytes[3] == 0x2D) return false;
         for (uint256 i; i < bytesLength; ) {
             bytes1 char = labelBytes[i];
             if (
