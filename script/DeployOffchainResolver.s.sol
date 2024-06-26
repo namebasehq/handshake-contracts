868bba8916dee7450c25bc79af35b72e2af63759
hodlthedoor <sam@piranha.me.uk>
2024-06-26 16:52:25 +0100

    updated deployment script for offchain resolver

--- script/DeployOffchainResolver.s.sol
+++ script/DeployOffchainResolver.s.sol
@@ -1,54 +1,42 @@
-//0x9b6435e0e73d40f8a64fe5094e4ea462a54a078b
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
-import {console} from "forge-std/console.sol";
-
-import "contracts/GlobalRegistrationRules.sol";
-import "contracts/HandshakeSld.sol";
-import "contracts/HandshakeTld.sol";
-import "contracts/LabelValidator.sol";
-import "contracts/metadata/GenericMetadata.sol";
-import "contracts/SldCommitIntent.sol";
-import "contracts/SldRegistrationManager.sol";
-import "contracts/DefaultRegistrationStrategy.sol";
-import "contracts/TldClaimManager.sol";
-import "contracts/UsdPriceOracle.sol";
-import "mocks/MockUsdOracle.sol";
-import "contracts/resolvers/DefaultResolver.sol";
-import "interfaces/IPriceOracle.sol";
-import "test/mocks/TestingRegistrationStrategy.sol";
+import "forge-std/console.sol";
 import "src/contracts/ccip/OffchainResolver.sol";
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract DeployOffchainResolverScript is Script {
     function setUp() public {
-
-
+        // Setup can be used to initialize contract instances, variables, etc.
     }
 
     function run() public {
+        vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
 
-        // forge script script/DeployOffchainResolver.s.sol:DeployOffchainResolverScript --private-key $GOERLI_DEPLOYER_PRIVATE_KEY --rpc-url $RPC_URL --broadcast -vv
-
-
-        vm.startBroadcast(vm.envUint("GOERLI_DEPLOYER_PRIVATE_KEY"));
-
-        string memory url = "https://hns-id-git-ccip-dns-gasfree-update-namebase.vercel.app/api/gateway/ccip?data={data}&sender={sender}";
-
+        string memory url;
         address[] memory signers = new address[](1);
+        address ens;
 
-        signers[0] = 0x9b6435E0E73d40F8A64fE5094e4ea462a54a078B;
+        uint256 chainId = block.chainid;
+        console.log("Deploying to chain ID:", chainId);
 
-        address ens = address(0x1);
-  
+        if (chainId == 1) { // Mainnet
+            url = "https://hns.id/api/gateway/ccip?sender={sender}&data={data}";
+            signers[0] = 0x9b6435e0e73d40f8a64fe5094e4ea462a54a078b;
+            ens = address(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e); // Mainnet ENS address
+        } else if (chainId == 11155111) { // Sepolia
+            url = "https://hnst.id/api/gateway/ccip?sender={sender}&data={data}";
+            signers[0] = 0xdEC45E28975FF38ACBb37Af9Eaa64493c3aDa126;
+            ens = address(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e); // sepolia address the same
+        } else {
+            revert("Unsupported network");
+        }
+
         // Deploy the off-chain resolver
         OffchainResolver offchainResolver = new OffchainResolver(url, signers, ens);
-
         console.log("OffchainResolver deployed at: ", address(offchainResolver));
 
-
+        vm.stopBroadcast();
     }
 }
