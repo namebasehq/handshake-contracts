d158f24b8a868d383bd58bcc79f8d44dcbcef0c1
hodlthedoor <sam@piranha.me.uk>
2024-08-16 12:20:34 +0100

    chore:(whns) Added optimism bridging contracts and erc-20 token for L2

---
+++ src/contracts/whns/whns-l2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import { OptimismMintableERC20 } from "./bedrock-contracts/OptimismMintableERC20.sol";
+
+/// @title Wrapped Handshake ERC20
+/// @notice Wrapped Handshake ERC20 is a token contract that inherits from OptimismMintableERC20
+///         and serves as the L2 representation of the WHNS token.
+contract WrappedHandshake is OptimismMintableERC20 {
+    /// @param _bridge      Address of the L2 standard bridge.
+    /// @param _remoteToken Address of the corresponding L1 token.
+    constructor(address _bridge, address _remoteToken)
+        OptimismMintableERC20(_bridge, _remoteToken, "Wrapped Handshake", "WHNS", 6)
+    {}
+}
